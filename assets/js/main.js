6f4ea89d1fc6fd53187496336c5c025e4f9d55d2
Jorge Luis De Gaetano <jdegaetano.seo@gmail.com>
2025-09-30 12:53:14 -0300

    Implementando el DarkMode

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,37 @@ function en_desarrollo()
 {
     alert('En desarrollo');
 }
+
+// Gestionar el modo oscuro 
+
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('toggle-dark-mode');
+    const icon = btn.querySelector('i');
+    const body = document.body;
+
+    // Cargar preferencia desde localStorage
+    const modoGuardado = localStorage.getItem('modo');
+    
+    if (modoGuardado === 'oscuro') 
+    {
+        body.classList.add('dark-mode');
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+
+    btn.addEventListener('click', () => {
+        body.classList.toggle('dark-mode');
+
+        const enModoOscuro = body.classList.contains('dark-mode');
+
+        if (enModoOscuro) {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+            localStorage.setItem('modo', 'oscuro');
+        } else {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+            localStorage.setItem('modo', 'claro');
+        }
+    });
+});
